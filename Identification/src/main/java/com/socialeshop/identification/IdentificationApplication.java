ec72763ea2067bdfa33bfbac324cc8c21ca66204
Yunzhe Jia <yj@MBP-Yunzhe.local>
2020-08-20 13:19:42 -0400

    start spring

---
+++ Identification/src/main/java/com/socialeshop/identification/IdentificationApplication.java
@@ -0,0 +1,13 @@
+package com.socialeshop.identification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentificationApplication.class, args);
+    }
+
+}
