3d99317845537833fb52b6ee892d8d484e31b5b4
Yunzhe Jia <yj@MBP-Yunzhe.local>
2020-08-27 16:47:42 -0400

    add demo vue project

---
+++ Identification/front_end_vue/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/logo.png"/>
+            Build-a-Bot
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <HomePage/>
+    </main>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+body {
+  background: linear-gradient(to bottom,#555,#999);
+  background-attachment: fixed;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+</style>
---
+++ Identification/front_end_vue/src/components/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/logo.png"  aria-hidden="true" />
+    </div>
+    <div class="get-started">
+      <a href="">Get started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+.robot {
+  height: 300px;
+}
+.get-started {
+  padding-top: 20px;
+  font-size: 25px;
+}
+</style>
