12f75ff15a37f27e2439bd60af123af05a139d65
Wanli Wang <wanliwang@WanlideMacBook-Pro.local>
2020-09-17 18:09:59 -0400

    ww: front_end_id_basic

---
+++ Identification/front_end/identification/src/profile/profilePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="HomePageContainer">
+        <h3 class="title">Login Success! Welcome!</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
