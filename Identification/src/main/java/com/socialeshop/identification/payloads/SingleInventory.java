22d5edbf5fee212eeaa5f7c2a3354356dbeeb227
Yunzhe Jia <yj@MBP-Yunzhe.local>
2020-10-22 19:57:45 -0400

    add getter and setter in singleInventory

--- Identification/src/main/java/com/socialeshop/identification/payloads/SingleInventory.java
+++ Identification/src/main/java/com/socialeshop/identification/payloads/SingleInventory.java
@@ -28,4 +28,52 @@ public class SingleInventory {
         this.amount = inventory.getAmount();
         this.description = inventory.getDescription();
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
