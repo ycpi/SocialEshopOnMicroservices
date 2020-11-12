713794687629b2bf333ec60048743314b2edf790
Zian Li <zianli@ZiandeMacBook-Pro.local>
2020-11-12 17:10:41 -0500

    debug:fixed amount issue in backend

--- Identification/front_end/identification/vue.config.js
+++ Identification/front_end/identification/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
       proxy: {
         '^/api': {
           //target: 'http://10.56.12.173:8080',
-          target: 'http://localhost:8081',
+          target: 'http://localhost:9401',
           pathRewrite: {'^/api': ''},
           changeOrigin: true,
           logLevel: "debug",
