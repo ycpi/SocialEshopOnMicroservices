1c07028865c0b95528a506c7ce307ec9a9b2e7fc
Wanli Wang <wanliwang@WanlideMacBook-Pro.local>
2020-11-12 21:36:33 -0500

    add:console-log

--- Identification/front_end/identification/vue.config.js
+++ Identification/front_end/identification/vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
     devServer: {
       proxy: {
         '^/api': {
-          //target: 'http://10.56.12.173:8080',
-          target: 'http://localhost:9401',
+          target: 'http://10.56.12.173:8080',
+          //target: 'http://localhost:9401',
           //target: 'http://localhost:9090',
           pathRewrite: {'^/api': ''},
           changeOrigin: true,
